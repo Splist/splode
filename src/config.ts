19d46617d2071b896ff0757615122f30bd0d1767
Jonah Snider <me@jonahsnider.ninja>
2019-10-03 16:02:06 -0700

    docs: add config docs

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,12 @@
+/**
+ * Project wide configuration.
+ */
 export interface Config {
+    /** GraphQL options */
     graphql: {
+        /** Whether or not to generate a schema */
         generateSchema?: boolean;
+        /** Whether or not to enable the playground */
         enablePlayground?: boolean;
     };
 }
